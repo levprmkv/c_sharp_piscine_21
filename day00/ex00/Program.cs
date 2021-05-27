5c1743dec6eb84e0de09e2e8e6f00e1e83ba2ab8
Bean Booster <bbooster@un-c1.kzn.21-school.ru>
2021-05-27 16:22:31 +0300

    firsrt commit

---
+++ day00/ex00/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static double DecrPay(int term, double sum, double payment, double annuity_payment, double i, 
+            int selectedMounth, DateTime now, double rate)
+        {
+            int day_m;
+            int k = 0;
+            double per;
+            double overpay = 0;
+
+            while (term != 0)
+            {
+                if (sum <= 0)
+                {
+                    sum = 0;
+                    break;
+                }
+                if (k == selectedMounth)
+                {
+                    sum -= payment;
+                    annuity_payment = (sum * i * (Math.Pow((1 + i), term))) / (Math.Pow((1 + i), term) - 1);
+                }
+                day_m = (now.AddMonths(k + 1) - now.AddMonths(k)).Duration().Days;
+                per = (sum * rate * day_m) / (100 * 365);
+                sum = sum - annuity_payment + per;
+                term--;
+                overpay += per;
+                k++;
+            }
+            return overpay;
+        }
+		static double RedTerm(int term, double sum, double payment, double annuity_payment, double i, 
+            int selectedMounth, DateTime now, double rate)
+		{
+			int day_m;
+            int k = 0;
+            double per;
+            double overpay = 0;
+
+			while (term != 0)
+            {
+                if (sum <= 0)
+                {
+                    sum = 0;
+                    break;
+                }
+                day_m = (now.AddMonths(k + 1) - now.AddMonths(k)).Duration().Days;
+                per = (sum * rate * day_m) / (100 * 365);
+                overpay += per;
+                sum = sum - annuity_payment + per;
+                if (k == selectedMounth - 1)
+                {
+                    term = (int) Math.Log((annuity_payment / (annuity_payment - i * sum)), (1 + i));
+                    sum -= payment;
+                }
+                term--;
+                k++;
+            }
+			return overpay;
+		}
+        
+        static void Main(string[] args)
+        {
+            double sum;
+            double rate;//процент годовой
+            int term;//месяц
+            int selectedMounth;//номер месяца досрочного платежа
+            double payment;//сумма досрочного платежв
+            double annuity_payment;
+            double i;//процентная ставка по займу в месяц
+            double overpayR;
+			double overpayD;
+            DateTime now = DateTime.Now;
+
+            if (args.Length != 5)
+            {
+                Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+                return;
+            }
+
+            double.TryParse(args[0], out sum);
+            double.TryParse(args[1], out rate);
+            Int32.TryParse(args[2], out term);
+            Int32.TryParse(args[3], out selectedMounth);
+            double.TryParse(args[4], out payment);
+
+            i = rate / 12 / 100;
+            annuity_payment = (sum * i * (Math.Pow((1 + i), term))) / (Math.Pow((1 + i), term) - 1);
+            overpayD = Math.Round(DecrPay(term, sum, payment, annuity_payment, i, selectedMounth, now,rate), 2);
+			overpayR = Math.Round(RedTerm(term, sum, payment, annuity_payment, i, selectedMounth, now,rate), 2);
+            Console.WriteLine("Переплата при уменьшении платежа: " + overpayD.ToString("0.00") + "p.");
+            Console.WriteLine("Переплата при уменьшении срока: " + overpayR.ToString("0.00") + "p.");
+
+			if (overpayD > overpayR)
+				Console.WriteLine("Уменьшение срока выгоднее уменьшения платежа на " + (overpayD - overpayR).ToString("0.00") + "p.");
+			else if (overpayD < overpayR)
+				Console.WriteLine("Уменьшение платежа выгоднее уменьшения срока на " + (overpayD - overpayR).ToString("0.00") + "p.");
+			else
+				Console.WriteLine("Переплата одинакова в обоих вариантах.");
+        }
+    }
+}
---
+++ day00/ex01/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Program
+{
+    class Program
+    {
+			static int LevenshteinDistance(string string1, string string2)
+		{
+			if (string1 == null) throw new ArgumentNullException("string1");
+			if (string2 == null) throw new ArgumentNullException("string2");
+			int diff;
+			int[,] m = new int[string1.Length + 1, string2.Length + 1];
+
+			for (int i = 0; i <= string1.Length; i++) 
+				m[i, 0] = i;
+			for (int j = 0; j <= string2.Length; j++)
+				m[0, j] = j;
+			for (int i = 1; i <= string1.Length; i++)
+			{
+				for (int j = 1; j <= string2.Length; j++)
+				{
+					if (string1[i - 1] == string2[j - 1])
+						diff = 0;
+					else
+						diff = 1;
+					m[i, j] = Math.Min(Math.Min(m[i - 1, j] + 1, m[i, j - 1] + 1), m[i - 1, j - 1] + diff);
+				}
+			}
+			return m[string1.Length, string2.Length];
+		}
+        static void Main()
+        {
+			int diff;
+			int i;
+			string ans;
+			int l_dist;
+			string name;
+			string[] line = File.ReadAllLines("us.txt");
+			Console.WriteLine("Enter name: ");
+			name = Console.ReadLine();
+			Console.WriteLine(name);
+			string patterns = "^[a-zA-Z]";
+			if (!Regex.IsMatch(name, patterns))
+			{
+				Console.WriteLine("Your name was not found.");
+				return ;
+			}
+
+			for (diff = 0; diff < 3; diff++)
+			{
+				for (i = 0; i < line.Length; i++)
+				{
+					l_dist = LevenshteinDistance(name, line[i]);
+					if (l_dist == diff)
+					{
+						if (l_dist == 0)
+						{
+							Console.WriteLine("Hello, " + name + "!");
+							return ;
+						}
+						Console.WriteLine("Did you mean \"" + line[i] + "\"? Y/N");
+						ans = Console.ReadLine();
+						while (ans != "Y" && ans != "N")
+						{
+							Console.WriteLine("Did you mean \"" + line[i] + "\"? Y/N");
+							ans = Console.ReadLine();
+						}
+						if (ans == "Y")
+						{
+							Console.WriteLine("Hello, " + line[i] + "!");
+							return ;
+						}
+					}
+				}
+			}
+			Console.WriteLine("Your name was not found.");
+			return ;
+        }
+    }
+}
